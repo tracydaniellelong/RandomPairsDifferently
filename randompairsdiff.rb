e451476f6e2d7066f8cfab8b2789cb69d4d83a4b
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-29 15:58:53 -0400

    fot it to work

--- randompairsdiff.rb
+++ randompairsdiff.rb
@@ -1,7 +1,11 @@
 def random_pair(pair)
 	ary = []
-	to_pair = pair.sample(2)
-	ary.push(to_pair)
-	p ary
-	
+	to_pair = pair.shuffle.each_slice(2)
+	to_pair.each do |x|
+		if x.length == 2
+			ary.push(x)
+		end
+	end
+	return ary
+
 end
