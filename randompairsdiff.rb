d625cde008e1505deb52a02df4b6064668149b7a
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-29 16:30:21 -0400

    my expected and actual assertions are wrong

--- randompairsdiff.rb
+++ randompairsdiff.rb
@@ -3,7 +3,7 @@ def random_pair(pair)
 	to_pair = pair.shuffle.each_slice(4)
 	to_pair.each do |x|
 		case 
-		when x.length == 2
+		when x.length == 4
 			ary.push(x)
 		when x.length == 1
 			ary[rand(ary.length)].concat(x)
