6a0dea976a2d9ad2c89acf34645a07001df2a6f0
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-29 16:36:23 -0400

    more tests

--- testrandompairsdiff.rb
+++ testrandompairsdiff.rb
@@ -2,16 +2,24 @@ require 'minitest/autorun'
 require_relative 'randompairsdiff.rb'
 
 class Testing < Minitest::Test
-	def test_pairs_1
+	def test_pairs_with_2_elements
 		pair = random_pair(["apple", "orange"])
 		assert_equal(0, pair.count)
 	end
-	def test_pairs_with_odd_count
+	def test_pairs_with_5_elements
 		pair = random_pair(["apple", "orange", "grape", "pear", "lemon"])
 		assert_equal(1, pair.count)
 	end
 	def test_pairs_with_4_elements
 		pair = random_pair(["apple", "orange", "grape", "pear", "lemon", "bush", "tree", "flower", "stick", "leaf", "dog", "cat", "mouse"])
 		assert_equal(3, pair.count)
+	end
+	def test_more_pairs_with_4_elements
+		pair = random_pair(["apple", "orange", "grape", "pear", "lemon", "bush", "tree", "flower", "stick", "leaf", "dog", "cat", "mouse", "bird", "moon", "lake", "fire", "rock", "sun"])
+		assert_equal(4, pair.count)
+	end
+	def test_more_pairs
+		pair = random_pair(["apple", "orange", "grape", "pear", "lemon", "bush", "tree", "flower", "stick", "leaf", "dog", "cat", "mouse", "bird", "moon", "lake", "fire", "rock", "sun", "sky", "cloud", "rain"])
+		assert_equal(5, pair.count)
 	end
 end
