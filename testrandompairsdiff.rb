4ef64ac1d56b6789ff9cc3703bbee6fc47d91f99
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-29 15:25:43 -0400

    defined a test for 1 pair

---
+++ testrandompairsdiff.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative 'randompairsdiff.rb'
+
+class Testing < Minitest::Test
+	def test_pairs_1
+		pair = create_random_pair(["apple", "orange"])
+		assert_equal(1, pair.count)
+	end
+end
