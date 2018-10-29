44831c64064f212c09608081734734bae029328c
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-29 16:38:28 -0400

    test for pairs of numbers, passed

--- testrandompairsdiff.rb
+++ testrandompairsdiff.rb
@@ -22,4 +22,8 @@ class Testing < Minitest::Test
 		pair = random_pair(["apple", "orange", "grape", "pear", "lemon", "bush", "tree", "flower", "stick", "leaf", "dog", "cat", "mouse", "bird", "moon", "lake", "fire", "rock", "sun", "sky", "cloud", "rain"])
 		assert_equal(5, pair.count)
 	end
+	def test_pairs_of_numbers
+		pair = random_pair([1, 2, 3, 4, 5, 6, 7, 8, 9])
+		assert_equal(2, pair.count)
+	end
 end
