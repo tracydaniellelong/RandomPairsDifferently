a2bda42ad499c7016cf193087db5af74527c8d1f
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-31 15:12:21 -0400

    made tests pass for pairs of 2

--- testrandompairsdiff.rb
+++ testrandompairsdiff.rb
@@ -8,23 +8,23 @@ class Testing < Minitest::Test
 	end
 	def test_pairs_with_5_elements
 		pair = random_pair(["apple", "orange", "grape", "pear", "lemon"])
-		assert_equal(1, pair.count)
+		assert_equal(2, pair.count)
 	end
 	def test_pairs_with_4_elements
 		pair = random_pair(["apple", "orange", "grape", "pear", "lemon", "bush", "tree", "flower", "stick", "leaf", "dog", "cat", "mouse"])
-		assert_equal(3, pair.count)
+		assert_equal(6, pair.count)
 	end
 	def test_more_pairs_with_4_elements
 		pair = random_pair(["apple", "orange", "grape", "pear", "lemon", "bush", "tree", "flower", "stick", "leaf", "dog", "cat", "mouse", "bird", "moon", "lake", "fire", "rock", "sun"])
-		assert_equal(4, pair.count)
+		assert_equal(9, pair.count)
 	end
 	def test_more_pairs
 		pair = random_pair(["apple", "orange", "grape", "pear", "lemon", "bush", "tree", "flower", "stick", "leaf", "dog", "cat", "mouse", "bird", "moon", "lake", "fire", "rock", "sun", "sky", "cloud", "rain"])
-		assert_equal(5, pair.count)
+		assert_equal(11, pair.count)
 	end
 	def test_pairs_of_numbers
 		pair = random_pair([1, 2, 3, 4, 5, 6, 7, 8, 9])
-		assert_equal(2, pair.count)
+		assert_equal(4, pair.count)
 	end
     
 end
